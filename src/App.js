a88ccb4a13d8ba43ce91427bfca0b8a8f111ee65
Saidul Mondal <mondalsaidul3232@gmail.com>
2021-03-23 17:18:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
+  const [otp, setOtp] = useState(new Array(4).fill(""))
+
+  const handleChange = (element, index) => {
+    if(isNaN(element.value)) return false ;
+
+    setOtp([...otp.map((d, idx) => idx === index ? element.value : d)])
+
+    if(element.nextSibling){
+      element.nextSibling.focus()
+    }
+  };
+
   return (
+    <div className="box">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>OTP-Mchine Test</h1>
+      {otp.map((data, index) => {
+         return( <input 
+           className="Otp"
+           type="text" 
+           name="otp"
+           maxLength="1" 
+           key={index} 
+           value={data}
+           onChange={(e) => handleChange(e.target, index) } 
+           onFocus={(e) => e.target.select()}
+            />)
+      })}
+      <br />
+      <button className="button" onClick={(e) => alert("Entered otp is " + otp.join(""))}>
+        Verify
+      </button>
+     <button className="clearOtp" onClick={(e) => setOtp([...otp].map(v => ""))}> Clear</button>
+       
+    </div>
     </div>
   );
 }
